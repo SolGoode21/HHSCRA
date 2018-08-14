7e8690d27bd67058c8ee2153cc4e3d3e22d901fe
SolGoode21 <solgoode21@outlook.com>
2018-08-14 14:11:28 -0500

    Add project files.

---
+++ HHSC_RA/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace HHSC_RA.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ HHSC_RA/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HHSC_RA.Startup))]
+namespace HHSC_RA
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
